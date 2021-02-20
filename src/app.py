0a6b07c1bb36deb960c954e8f72a2aa5a804568a
alejojimenez <ajimenez.rengifo@gmail.com>
2021-02-20 13:11:04 +0000

    Subiendo Proyecto python flask api

---
+++ src/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+from flask import request
+from flask import json
+app = Flask(__name__)
+
+global todos
+todos = [ { "label": "My first task", "done": False } ]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+  return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    todos.append(decoded_object)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods= ['DELETE'])
+def delete_todo(position=0):
+    todos.pop(position)
+    return jsonify(todos)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
